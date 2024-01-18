7dcf39d59e924b64356b452bcc006c5f62e706c9
ajnaje <kendicajna222@gmail.com>
2024-01-18 17:04:15 +0100

    Vj1

---
+++ Vj1-Components/src/App.jsx
@@ -0,0 +1,26 @@
+
+/*
+ZADATAK:
+
+Napravite novi branch sa svojim imenom, i u njemu radite
+
+Napravite novi folder "components" u folder u src i napravite 3 komponente:
+
+Header
+Body
+Footer
+
+Unutar njih nek bit samo div i untutar njega h1 u kojem pise Header/Body/Footer
+
+Napravite Pull Request
+*/
+
+function App() {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default App
